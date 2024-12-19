0bbd45c3f8b85d75c2b50b1e33637fe4ec2c0f62
r0mA2 <roma.slizhik@mail.ru>
2024-12-19 18:50:51 +0700

    Update Account.h

--- Account.h
+++ Account.h
@@ -1,1 +1,21 @@
 
+#ifndef ACCOUNT_H
+#define ACCOUNT_H
+
+#include <iostream>
+
+class Account {
+protected:
+    double balance;
+public:
+    Account(double initialBalance);
+    virtual void Deposit(double amount);
+    virtual void Withdraw(double amount) = 0;
+    double GetBalance() const;
+    virtual void DisplayInfo() const = 0;
+    virtual ~Account() = default;
+
+    friend std::ostream& operator<<(std::ostream& os, const Account& account);
+};
+
+#endif
